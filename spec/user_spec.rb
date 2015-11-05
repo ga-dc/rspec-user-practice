d2d387169704b5a82eff2eecdadd5c0d571efd37
RobertAKARobin <robertgfthomas@gmail.com>
2015-11-05 16:32:37 -0500

    starter code

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,63 +4,23 @@ require_relative "../models/user"
 
 RSpec.describe User do
   describe "names" do
-    it "#name returns name when set" do
-      bob = User.new(name: "Bob", username: "bdylan" )
-      expect(bob.name).to eq("Bob")
-    end
-
-    it "#name returns username when no name is set" do
-      bob = User.new(name: nil, username: "bdylan" )
-      expect(bob.name).to eq("bdylan")
-    end
-
+    it "#name returns name when set"
+    it "#name returns username when no name is set"
     context "with two words in the name" do
-      let(:bob) { User.new(name: "Bob Smith")}
-
-      it "#first_name returns the first word in the name" do
-        expect(bob.first_name).to eq("Bob")
-      end
-
-      it "#last_name returns the last word in the name" do
-        expect(bob.last_name).to eq("Smith")
-      end
+      it "#first_name returns the first word in the name"
+      it "#last_name returns the last word in the name"
     end
-
     context "with 3+ words in the name" do
-      let(:charlie) { User.new(name: "Charlie J. Smith")}
-
-      it "#first_name returns the first word in the name" do
-        expect(charlie.first_name).to eq("Charlie")
-      end
-
-      it "#last_name returns the last word in the name name" do
-        expect(charlie.last_name).to eq("Smith")
-      end
+      it "#first_name returns the first word in the name"
+      it "#last_name returns the last word in the name name"
     end
-
     context "with improper capitalization" do
-      let(:david) { User.new(name: "david smith")}
-
-      it "#first_name returns the first word in the name" do
-        expect(david.first_name).to eq("David")
-      end
-
-      it "#last_name returns the last word in the name name" do
-        expect(david.last_name).to eq("Smith")
-      end
+      it "#first_name returns the first word in the name"
+      it "#last_name returns the last word in the name name"
     end
-
     context "with no name" do
-      let(:bob) { User.new(name: nil, username: "bob")}
-
-      it "#first_name returns username" do
-        expect(bob.first_name.downcase).to eq(bob.username.downcase)
-      end
-
-      it "#last_name returns username" do
-        expect(bob.last_name.downcase).to eq(bob.username.downcase)
-      end
+      it "#first_name returns username"
+      it "#last_name returns username"
     end
-
   end
 end
